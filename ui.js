92548cf94c1e187dc7530625891c7d6ca47addc7
Jorge Ramirez <jwramirezc@gmail.com>
2024-10-22 11:32:37 -0500

    correccion de console log empty

--- ui.js
+++ ui.js
@@ -63,7 +63,7 @@ export class UI {
 
   static showDeleteButton(deleteAll) {
     if (localStorage.getItem('tasks') !== '[]') {
-      console.log('empty');
+      // console.log('empty');
       deleteAll.style.display = 'block';
     }
   }
